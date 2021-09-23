0c45f10e3989b109ef6e4aea99db7b2f1a14ac73
seekersoftec <seekersoftec@gmail.com>
2021-09-23 13:15:09 +0100

    Working on TA Analyzer

--- analysis/ta_analyser.py
+++ analysis/ta_analyser.py
@@ -2,68 +2,57 @@
 import btalib
 import pandas as pd
 
-#
-#
-#
 
+class TechnicalAnalyser:
+    """
+        Technical Analysis master analyser
+        Class for calculating technical analysis indicators.
+
+        Get technical analyses based on 100+(120) indicators,
+        56 canlestick pattern detectors and 32 strategy alerts.
+    """
+
+    def __init__(self, dataframe: pd.DataFrame) -> None:
+        self.dataframe = dataframe
+        self.ohlcv = ['open', 'high', 'low', 'close', 'volume']
+        # Filter some indicators
+        self.filtered_indicators = []
+        for indicator in btalib.get_indicators():
+            # print(getattr(indicator, 'alias', ())[0])
+            for input_val in getattr(indicator, 'inputs', ()):
+                if input_val in self.ohlcv:
+                    self.filtered_indicators.append(indicator)
+
+    def eval_indicators(self, **kwargs) -> pd.DataFrame:
+        """ Evaluates TA algorithms and returns a dataframe """
+        for indicator in self.filtered_indicators:
+            try:
+                self.dataframe = self.dataframe.combine_first(
+                    indicator(self.dataframe).df)
+            except Exception:
+                pass
+        return self.dataframe
+
+
+#
+#
 #
 #
 # Read a csv file into a pandas dataframe
-df = pd.read_csv('../data/BTCUSDT-1d-binance_data.csv',
+df = pd.read_csv('data/BTCUSDT-1d-binance_data.csv',
                  parse_dates=True, index_col='timestamp')
-# df.index = pd.to_datetime(df.index, unit='ms')
+df.index = pd.to_datetime(df.index, unit='ms')
+#
+#
 # create sma and attach as column to original df
-df['20sma'] = btalib.sma(df.close, period=20).df
-
-rsi = btalib.rsi(df, period=14).df
-# print(rsi.df.rsi[-1])
-macd = btalib.macd(df, pfast=20, pslow=50, psignal=13).df
-df = df.join([rsi, macd])
-
+# df['20sma'] = btalib.sma(df.close, period=20).df
+#
+# rsi = btalib.rsi(df, period=14).df
+# # print(rsi.df.rsi[-1])
+# macd = btalib.macd(df, pfast=20, pslow=50, psignal=13).df
+# df = df.join([rsi, macd])
 #
 #
-indicators = btalib.get_indicators()
-# ac = indicators[0](df)
-# print(indicators[0].__doc__)
-
-# print(df.tail(5))
-#
-#
-#
-#
-
-#
-# TA master analyser
-#
-
-
-class TechnicalAnalyser():
-    """ class for calculating technical analysis indicators """
-
-    def __init__(self, dataframe: pd.DataFrame):
-        pass
-
-    def eval(self, **kwargs) -> pd.DataFrame:
-        """ Evaluates TA algorithms and returns a dataframe """
-        return
-
-
-#
-#
-class MI(btalib.Indicator.MetaIndicator):
-    def __init__(self):
-        super().__init__()
-
-    def ve(self):
-        pass
-
-
-#
-#
-# Get technical analyses based on 120 indicators,
-# 56 canlestick pattern detectors, 32 strategy alerts that are not provided by any other platform.
-#
-#
-#
-#
-#
+ta_analyzer = TechnicalAnalyser(df)
+ini = ta_analyzer.eval_indicators()
+print(ini.tail(5))
